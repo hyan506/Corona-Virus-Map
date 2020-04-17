17cfe899a94b8c2341039f2839187e16cbf26a49
Steven Yan <hyan506@aucklanduni.ac.nz>
2020-04-18 04:12:31 +1200

    Updates

---
+++ precache-manifest.7fff9e91aade2e23e9ac50ee29467dd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fe5812a386f79f961a5f143c48e6f1",
+    "url": "/Corona-Virus-Map/index.html"
+  },
+  {
+    "revision": "ce2acb90bc927ab251b0",
+    "url": "/Corona-Virus-Map/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "57e242e6817e1945d921",
+    "url": "/Corona-Virus-Map/static/css/main.9b5cc161.chunk.css"
+  },
+  {
+    "revision": "ce2acb90bc927ab251b0",
+    "url": "/Corona-Virus-Map/static/js/2.13582868.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/Corona-Virus-Map/static/js/2.13582868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e242e6817e1945d921",
+    "url": "/Corona-Virus-Map/static/js/main.2e34bed8.chunk.js"
+  },
+  {
+    "revision": "c01fde64f512f8eb714b",
+    "url": "/Corona-Virus-Map/static/js/runtime-main.bbdd21ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-tracker/precache-manifest.8fb1c908ed2055b7e2d4fe4b65fb0dc4.js"
+  "/Corona-Virus-Map/precache-manifest.7fff9e91aade2e23e9ac50ee29467dd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Corona-Virus-Map/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
