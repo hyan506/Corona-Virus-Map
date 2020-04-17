54ad5af840d6ccc5f634952ae21a1b2e9d896485
Steven Yan <hyan506@aucklanduni.ac.nz>
2020-04-18 03:45:00 +1200

    Updates

---
+++ precache-manifest.8fb1c908ed2055b7e2d4fe4b65fb0dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5787dcdb974745fc121289a160da065b",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "d866b30b19401fdd2299",
+    "url": "/corona-tracker/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "e2a9af5df052fba7424a",
+    "url": "/corona-tracker/static/css/main.9b5cc161.chunk.css"
+  },
+  {
+    "revision": "d866b30b19401fdd2299",
+    "url": "/corona-tracker/static/js/2.df568e54.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/corona-tracker/static/js/2.df568e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a9af5df052fba7424a",
+    "url": "/corona-tracker/static/js/main.34fb5d3e.chunk.js"
+  },
+  {
+    "revision": "69a10effee46d655a76b",
+    "url": "/corona-tracker/static/js/runtime-main.b1278e06.js"
+  }
+]);
